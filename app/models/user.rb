8b386e79889459a762e7b6f38be073ec78ed5157
pkim050 <pkim050@gmail.com>
2019-05-28 14:18:41 -0700

    Omniauth github complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,9 @@ class User < ApplicationRecord
   has_many :gyms, through: :reservations, dependent: :destroy
 
   def self.from_omniauth(auth)
-    
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      #binding.pry
       user.provider = auth.provider
-      user.first_name = auth.info.nickname
+      user.username = auth.info.nickname
       user.uid = auth.uid
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
--- db/schema.rb
+++ db/schema.rb
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2019_05_28_180516) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "username"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
