b3d0246461575704f86fc499541963684aabb00e
pa-kim <pa_kim@apple.com>
2021-06-12 08:26:28 -0700

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,10 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  AirGnB::Application.configure do
-    config.assets.compile = true
-  end
-
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -31,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
