7908a5f04bb45f1bcd86b049bf893d5bd735be43
pa-kim <pa_kim@apple.com>
2021-06-12 07:41:03 -0700

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,10 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  air-gnb::Application.configure do
+    config.assets.compile = true
+  end
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
